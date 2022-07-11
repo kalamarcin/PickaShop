97c32a4e64a6b0cb29fd680460b571ceed96987e
kalamarcin <kalamarcin1@gmail.com>
2022-07-11 19:07:35 +0200

    first JS

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function (e){
+  e.preventDefault();
+
+  const data = new FormData(form);
+  console.log([...data])
+  
+})
