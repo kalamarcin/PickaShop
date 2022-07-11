aaaf71a3917d5c1134b8a32f4a21b7810e15295d
kalamarcin <kalamarcin1@gmail.com>
2022-07-11 22:47:16 +0200

    FormData struggle

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
+const btnReset = function () {
+  document.getElementById("form").reset()
+};
 
+clearBtn = document.getElementById('delate-btn');
+
+clearBtn.addEventListener('click', btnReset)
 const form = document.getElementById('form');
+const namef = document.getElementById('fname');
+const email = document.getElementById('email');
+const telefon = document.getElementById('telefon');
+const adres = document.getElementById('adres');
+const errorElement = document.getElementById('error');
 
-form.addEventListener('submit', function (e){
-e.preventDefault();
+form.addEventListener('submit', (e) =>{
+  e.preventDefault();
 
-console.log([...data])
-})
+  const data = new FormData(form);
+  
+console.log(...data)
+});
+
+
+
+
+
