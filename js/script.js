f9dbc6bcc0558b767b342313df848331acc700dc
kalamarcin <kalamarcin1@gmail.com>
2022-07-14 22:20:35 +0200

    second validation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 const btnReset = function () {
   document.getElementById("form").reset()
 };
@@ -15,12 +13,47 @@ const max = 2;
 function selectiveCheck(event) {
   const checkedChecks = document.querySelectorAll(".check:checked");
   if (checkedChecks.length >= max + 1)
-    return false;
-}
+    return false
+};
 
 for (let i = 0; i < checks.length; i++)
   checks[i].onclick = selectiveCheck;
 
+
+function validation() {
+  const fname = document.getElementById('fname').value;
+  const email = document.getElementById('email').value;
+  const telefon = document.getElementById('telefon').value;
+  const fnameBox = document.getElementById('fname-box');
+  const emailBox = document.getElementById('email-box');
+  const telefonBox = document.getElementById('telefon-box');
+  
+  const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const numberPattern = /^[0-9]+$/;
+
+if (fname.length < 6) {
+  fnameBox.classList.add('invalid');
+  fnameBox.classList.remove('valid');
+} else {
+  fnameBox.classList.add('valid');
+  fnameBox.classList.remove('invalid');
+}
+
+ if (email.match(emailPattern)) {
+  emailBox.classList.add('valid');
+  emailBox.classList.remove('invalid');
+} else {
+  emailBox.classList.add('invalid');
+  emailBox.classList.remove('valid');
+}
+
+if (telefon.match(numberPattern)) {
+  telefonBox.classList.add('valid');
+  telefonBox.classList.remove('invalid');
+} else {
+  telefonBox.classList.add('invalid');
+  telefonBox.classList.remove('valid');
+}};
 
 
 
@@ -32,19 +65,23 @@ form.addEventListener('submit', (e) => {
   const fname = document.getElementById('fname').value.trim();
   const email = document.getElementById('email').value.trim();
   const telefon = document.getElementById('telefon').value.trim();
+  const pizza = document.getElementById('pizza').value;
   const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   const regex = /^[0-9]+$/;
+
   const errors = [];
 
-  if (fname < 6) {
+  if (!telefon.match(regex)) {
+    errors.push('Nieprawidłowy numer telefonu')
+  }
+  if (!email.match(emailPattern)) {
+    errors.push('Nieprawidłowy adres Email')
+  }
+  if (fname.length < 6) {
     errors.push('Pole "Imię i Nazwisko" musi zawierac co najmniej 6 znaków')
   }
-  
-  if (!email.match(emailPattern)) {
-    errors.push('Nieprawidłowy adres Email');
-  }
-  if (!telefon.match(regex)) {
-    errors.push('Nieprawidłowy numer telefonu');
+  if (pizza == ""){
+    errors.push('Wybierz pickę!')
   }
   if (errors.length > 0) {
     for(let i = 0; i < errors.length; i++){
@@ -70,13 +107,6 @@ form.addEventListener('submit', (e) => {
         }
     }).showToast();
   }
-
-
-
-
-
-
-
 
   const data = new FormData(form);
   for (const [key, value] of data) {
