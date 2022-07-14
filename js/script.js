354ef86d80321df19346278a387586e21df94f5b
kalamarcin <kalamarcin1@gmail.com>
2022-07-14 13:10:27 +0200

    checkbox validation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 const btnReset = function () {
   document.getElementById("form").reset()
 };
@@ -12,13 +14,40 @@ const telefon = document.getElementById('telefon');
 const adres = document.getElementById('adres');
 const errorElement = document.getElementById('error');
 
+const checks = document.querySelectorAll(".check");
+const max = 2;
+
+function selectiveCheck (event) {
+  const checkedChecks = document.querySelectorAll(".check:checked");
+  if (checkedChecks.length >= max + 1)
+    return false;
+}
+
+for (let i = 0; i < checks.length; i++)
+  checks[i].onclick = selectiveCheck;
+
+
+
+
 form.addEventListener('submit', (e) =>{
   e.preventDefault();
 
-  const data = new FormData(form);
   
-console.log(...data)
+
+
+  const data = new FormData(form);
+  for (const [key, value] of data) {
+    console.log(`${key}: ${value}\n`)
+   }
 });
+
+
+
+
+
+
+
+
 
 
 
